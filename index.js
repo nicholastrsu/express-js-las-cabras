6272029ad7f72dbc21f481ab1dec0dd5cda8395f
juanpena1 <juanpenlo@unisabana.edu.co>
2024-09-13 22:17:59 -0500

    Primer cambio

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000, ()=>{
+    console.log('Server is  running  on  port 3000')
+})
+
+
+const students ={
+    1:{
+        name: "Juan",
+        lastName: "Peña",
+        email: "juanpenlo@unisabana.edu.co",
+        id: "0000303610"
+    }
+}
+app.get('/user-info/:id', (req, res) => {
+    const studentId = req.params.id;
+
+    if (students[studentId]) {
+        res.json(students[studentId]);
+      } else {
+        res.status(404).json({ message: "Estudiante no encontrado" });
+      }
+    });
