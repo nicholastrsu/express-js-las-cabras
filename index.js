05f2544c0c0a78d8eda785ab14a64e4d79c065be
nicholas <nicholastrsu@unisabana.edu.co>
2024-09-24 16:42:11 -0500

    ,,,,

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-app.use(cors());
+
 const express = require('express')
 const app = express()
 
 app.listen(3000, ()=>{
     console.log('Server is  running  on  port 3000')
 })
-
+app.use(cors());
 
 const students ={
     1:{
